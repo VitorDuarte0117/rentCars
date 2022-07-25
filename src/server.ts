134c0e55700b06fe9de1d9a3f58f743f93f10ce5
vitor <vitorduarte.vds7@gmail.com>
2022-07-25 11:31:45 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello world" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+  return res.json({ name });
+});
+
+app.listen(3000, () => {
+  console.log("Ta rodando");
+});
